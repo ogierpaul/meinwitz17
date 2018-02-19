b2ed2b9394c2e9b82f7e3fbbbcde9492d187c2b4
ogierpaul <ogier.paul@gmail.com>
2018-02-19 16:41:47 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='meinwitz17',
+      version ='0.1',
+      description='die beste witz in der welt',
+      author='Paul Ogier',
+      author_email='ogier.paul@gm',
+      license='MIT',
+      packages=['meinwitz17'],
+      zip_safe=False)
